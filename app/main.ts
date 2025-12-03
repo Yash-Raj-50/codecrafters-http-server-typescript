59454879d5f76830798d4898d29c04f528a56b08
Yash-Raj-50 <yashraj44777@gmail.com>
2025-12-03 12:52:54 +0530

    200 default response added

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,11 @@
 import * as net from "net";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+console.log("Logs from your program will appear here! achchha");
 
 // TODO: Uncomment the code below to pass the first stage
 const server = net.createServer((socket) => {
+  socket.write('HTTP/1.1 200 OK\r\n\r\n');
   socket.on("close", () => {
     socket.end();
   });
