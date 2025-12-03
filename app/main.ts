cd44039f87af2ba48e16d0eae7e8b7734a3c07ec
Yash-Raj-50 <yashraj44777@gmail.com>
2025-12-03 12:58:30 +0530

    200 response close connection fix

--- app/main.ts
+++ app/main.ts
@@ -6,9 +6,10 @@ console.log("Logs from your program will appear here! achchha");
 // TODO: Uncomment the code below to pass the first stage
 const server = net.createServer((socket) => {
   socket.write('HTTP/1.1 200 OK\r\n\r\n');
-  socket.on("close", () => {
-    socket.end();
-  });
+  socket.end();
+  // socket.on("close", () => {
+  //   socket.end();
+  // });
 });
 
 server.listen(4221, "localhost");
